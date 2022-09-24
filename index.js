5e5686da41a803037ebaadb1865c11744afb5bc2
Miquel Alejandro Vicens Dalmau <miquelvd86@gmail.com>
2022-09-24 19:13:35 +0200

    Improve source code

--- index.js
+++ index.js
@@ -14,30 +14,31 @@ const gulp = require('gulp'),
 
 let domain = undefined;
 
-const allFiles = getFiles(),
+const allFiles = getGlob(),
 	indexHtmlFile = 'index.html',
+	jsFiles = getGlob('js'),
 	cssFilename = 'index',
-	cssFullFilename = cssFilename + '.css';
-let stylesFolder = 'styles/',
+	cssFile = cssFilename + '.css';
+let stylesDir = 'styles/',
 	jsTemplatesFile = 'scripts/templates.js';
 
-const paths = {
+const globs = {
 	src: 'src/',
 	tmp: '.tmp/',
 	dist: 'dist/'
 };
-paths.srcIndexHtml = paths.src + indexHtmlFile;
-paths.srcCss = paths.src + stylesFolder + cssFullFilename;
-paths.srcLess = paths.src + getFiles('less');
-paths.srcSass = paths.src + getFiles('scss');
-paths.srcJs = paths.src + getFiles('js');
-paths.srcOthers = [paths.src + allFiles, '!' + paths.srcIndexHtml, '!' + paths.srcCss, '!' + paths.srcLess, '!' + paths.srcSass, '!' + paths.srcJs];
+globs.srcIndexHtml = globs.src + indexHtmlFile;
+globs.srcJs = globs.src + jsFiles;
+globs.srcIndexAndSrcJs = [globs.srcIndexHtml, globs.srcJs];
+globs.srcStyles = [globs.src + stylesDir + cssFile, globs.src + getGlob('less'), globs.src + getGlob('scss')];
+globs.srcOthers = [globs.src + allFiles, ...[...globs.srcIndexAndSrcJs, ...globs.srcStyles].map(glob => '!' + glob)];
+globs.tmpAllFiles = globs.tmp + allFiles;
 
 const nl = '\n',
 	tab = '	';
 
 function clean() {
-	return delFolder(paths.tmp);
+	return delDir(globs.tmp);
 }
 
 function manageDomain() {
@@ -54,25 +55,25 @@ function manageDomain() {
 		domain = domains[domainIndex];
 	}
 	const isMatched = domain !== undefined;
-	return gulp.src(paths.src)
+	return gulp.src(globs.src)
 		.pipe($.notify(isMatched ? `Matching domain: "${domainIndex}".` : 'No domain matched.'));
 }
 manageDomain.displayName = 'manage-domain';
 
 function buildIndex() {
-	return gulp.src(paths.srcIndexHtml)
-		.pipe(injStr.after('<!-- endbuild -->', nl + tab + `<link rel="stylesheet" href="${stylesFolder}${cssFullFilename}">`))
-		.pipe($.inject(gulp.src(paths.srcJs).pipe($.angularFilesort()), { relative: true })).on('error', notifyError)
+	return gulp.src(globs.srcIndexHtml)
+		.pipe(injStr.after('<!-- endbuild -->', nl + tab + `<link rel="stylesheet" href="${stylesDir + cssFile}">`))
+		.pipe($.inject(gulp.src(globs.srcJs).pipe($.angularFilesort()), { relative: true })).on('error', notifyError)
 		.pipe($.wiredep())
 		.pipe($.useref())
-		.pipe(gulp.dest(paths.tmp));
+		.pipe(gulp.dest(globs.tmp));
 }
 buildIndex.displayName = 'build-index';
 
 function injectDomain() {
-	return gulp.src(paths.tmp + getFiles('js'))
+	return gulp.src(globs.tmp + jsFiles)
 		.pipe(injStr.replace('{{AUTOFRONT_DOMAIN}}', domain))
-		.pipe(gulp.dest(paths.tmp));
+		.pipe(gulp.dest(globs.tmp));
 }
 injectDomain.displayName = 'inject-domain';
 
@@ -80,12 +81,12 @@ const indexAndJs = gulp.series(buildIndex, injectDomain);
 
 function styles() {
 	return mergeStream(getStream('css'), getStream('less', $.less), getStream('scss', gulpSass, '@import "variables";'))
-		.pipe($.concat(cssFullFilename))
-		.pipe(gulp.dest(paths.tmp + stylesFolder))
+		.pipe($.concat(cssFile))
+		.pipe(gulp.dest(globs.tmp + stylesDir))
 		.pipe(browserSync.stream());
 
 	function getStream(ext, process, extraCode) {
-		let stream = gulp.src(paths.src + stylesFolder + cssFilename + '.' + ext, { allowEmpty: true });
+		let stream = gulp.src(globs.src + stylesDir + cssFilename + '.' + ext, { allowEmpty: true });
 		if (process)
 			return stream
 				.pipe(injStr.prepend((extraCode ? extraCode + nl : '') + '// bower:' + ext + nl + '// endbower' + nl))
@@ -98,20 +99,20 @@ function styles() {
 function fonts() {
 	return gulp.src(mainBowerFiles())
 		.pipe(filter(['eot', 'otf', 'svg', 'ttf', 'woff', 'woff2'], true))
-		.pipe(gulp.dest(paths.tmp + 'fonts/'));
+		.pipe(gulp.dest(globs.tmp + 'fonts/'));
 }
 
 function others() {
 	const pugFilter = filter('pug');
-	return gulp.src(paths.srcOthers)
+	return gulp.src(globs.srcOthers)
 		.pipe(pugFilter).pipe($.pug()).on('error', notifyError).pipe(pugFilter.restore)
-		.pipe(gulp.dest(paths.tmp));
+		.pipe(gulp.dest(globs.tmp));
 }
 
 function about() {
 	return gulp.src('package.json')
 		.pipe($.about())
-		.pipe(gulp.dest(paths.tmp));
+		.pipe(gulp.dest(globs.tmp));
 }
 
 const buildTmp = gulp.series(
@@ -120,19 +121,19 @@ const buildTmp = gulp.series(
 );
 
 function browser(cb) {
-	browserSyncInit(paths.tmp);
+	browserSyncInit(globs.tmp);
 	cb();
 }
 
 function watch() {
-	gulp.watch([paths.srcIndexHtml, paths.srcJs], indexAndJs);
-	gulp.watch([paths.srcCss, paths.srcLess, paths.srcSass], styles);
-	gulp.watch(paths.srcOthers, others).on('unlink', function (path) {
-		gulp.src(path.replaceAll('\\', '/').replace(paths.src, paths.tmp).replace('.pug', '.html'))
+	gulp.watch(globs.srcIndexAndSrcJs, indexAndJs);
+	gulp.watch(globs.srcStyles, styles);
+	gulp.watch(globs.srcOthers, others).on('unlink', function (path) {
+		gulp.src(path.replaceAll('\\', '/').replace(globs.src, globs.tmp).replace('.pug', '.html'))
 			.pipe($.clean());
-		deleteEmpty(paths.tmp);
+		deleteEmpty(globs.tmp);
 	});
-	gulp.watch([paths.tmp + allFiles, '!' + paths.tmp + stylesFolder + cssFullFilename], function (cb) {
+	gulp.watch([globs.tmpAllFiles, '!' + globs.tmp + stylesDir + cssFile], function (cb) {
 		browserSync.reload();
 		cb();
 	});
@@ -141,26 +142,26 @@ function watch() {
 gulp.task('serve', gulp.series(buildTmp, browser, watch));
 
 function cleanDist() {
-	return delFolder(paths.dist);
+	return delDir(globs.dist);
 }
 cleanDist.displayName = 'clean:dist';
 
 function copy() {
-	return gulp.src(paths.tmp + allFiles)
-		.pipe(gulp.dest(paths.dist));
+	return gulp.src(globs.tmpAllFiles)
+		.pipe(gulp.dest(globs.dist));
 }
 
 function buildTemplates() {
-	return gulp.src([paths.dist + getFiles('html'), '!' + paths.dist + indexHtmlFile])
-		.pipe($.cleanDest(paths.dist))
+	return gulp.src([globs.dist + getGlob('html'), '!' + globs.dist + indexHtmlFile])
+		.pipe($.cleanDest(globs.dist))
 		.pipe(minifyHtml())
 		.pipe($.angularTemplatecache(jsTemplatesFile, { module: 'app', transformUrl: function (url) { return url.slice(1); } }))
-		.pipe(gulp.dest(paths.dist));
+		.pipe(gulp.dest(globs.dist));
 }
 buildTemplates.displayName = 'build-templates';
 
 function cleanTemplates() {
-	return deleteEmpty(paths.dist);
+	return deleteEmpty(globs.dist);
 }
 cleanTemplates.displayName = 'clean-templates';
 
@@ -171,7 +172,7 @@ function finishBuild() {
 		cssAndJsFilter = filter(['css', 'js']),
 		imgFilter = filter(['png', 'jpg', 'gif', 'svg']),
 		jsonFilter = filter('json');
-	return gulp.src(paths.dist + allFiles)
+	return gulp.src(globs.dist + allFiles)
 		.pipe(indexHtmlFilter).pipe(injStr.before('</body>', `<script src="${jsTemplatesFile}"></script>` + nl)).pipe(minifyHtml()).pipe(indexHtmlFilter.restore)
 		.pipe(cssFilter).pipe($.cssnano({ zindex: false })).pipe(cssFilter.restore)
 		.pipe(jsFilter).pipe($.ngAnnotate()).pipe($.terser()).pipe(jsFilter.restore)
@@ -180,14 +181,14 @@ function finishBuild() {
 		.pipe(imgFilter).pipe($.imagemin()).pipe(imgFilter.restore)
 		.pipe(jsonFilter).pipe($.jsonmin()).pipe(jsonFilter.restore)
 		.pipe($.size({ showFiles: true }))
-		.pipe(gulp.dest(paths.dist));
+		.pipe(gulp.dest(globs.dist));
 }
 finishBuild.displayName = 'finish-build';
 
 gulp.task('build', gulp.series(buildTmp, cleanDist, copy, buildTemplates, cleanTemplates, finishBuild));
 
 function browserDist(cb) {
-	browserSyncInit(paths.dist);
+	browserSyncInit(globs.dist);
 	cb();
 }
 browserDist.displayName = 'browser:dist';
@@ -197,31 +198,29 @@ gulp.task('serve:dist', gulp.series('build', browserDist));
 gulp.task('default', gulp.task('serve'));
 
 function html5Mode() {
-	const pathPrefix = '/';
-	stylesFolder = pathPrefix + stylesFolder;
-	jsTemplatesFile = pathPrefix + jsTemplatesFile;
+	const prefix = '/';
+	stylesDir = prefix + stylesDir;
+	jsTemplatesFile = prefix + jsTemplatesFile;
 }
 
-function getFiles(ext = '*') {
-	const isArray = typeof ext != 'string';
-	return '**/*.' + (isArray ? '{' : '') + (isArray ? ext.join() : ext) + (isArray ? '}' : '');
+function getGlob(ext = '*') {
+	const glob = '**/*.';
+	if (typeof ext == 'string')
+		return glob + ext;
+	return glob + '{' + ext.join() + '}';
 }
 
-function delFolder(path) {
-	return gulp.src(path, { allowEmpty: true, read: false })
+function delDir(glob) {
+	return gulp.src(glob, { allowEmpty: true, read: false })
 		.pipe($.clean());
 }
 
 function filter(ext, isUnrestored) {
-	return $.filter(getFiles(ext), { restore: !isUnrestored });
+	return $.filter(getGlob(ext), { restore: !isUnrestored });
 }
 
 function browserSyncInit(path) {
-	browserSync.init({
-		server: {
-			baseDir: path
-		}
-	});
+	browserSync.init({ server: path });
 }
 
 function minifyHtml() {
